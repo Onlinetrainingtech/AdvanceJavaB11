0b1f3c9c688af84ed0da4991531d4471eaa5628e
OnlineTrainer <mohamedtechdata@gmail.com>
2022-05-13 22:21:17 +0530

    13-05-2022

---
+++ AdJavaP2/src/main/java/loginservlet.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginservlet
+ */
+@WebServlet("/loginservlet")
+public class loginservlet extends HttpServlet
+{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out=resp.getWriter();
+		String u1=req.getParameter("uname");
+		String p1=req.getParameter("pass");
+		
+		if(u1.equals("admin")&&p1.equals("admin@123"))
+		{
+			RequestDispatcher rd=req.getRequestDispatcher("sucess.html");
+			rd.forward(req, resp);
+			//out.println("LoginSucess!!!");
+		}
+		else
+		{
+			RequestDispatcher rd=req.getRequestDispatcher("error.html");
+			rd.include(req, resp);
+			//out.println("LoginFail!!!");
+		}
+		
+		//out.println("Your data is::"+u1+""+p1);
+		
+	}
+
+}
